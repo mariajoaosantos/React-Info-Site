0faa29a0e4021c2732c6c16d4da91ea71a672d42
mariajoaosantos <mariajcs18@gmail.com>
2022-06-13 16:08:29 +0100

    first commit

---
+++ my-app/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function NavBar() {
+  return (
+    <nav className="nav">
+      <div className="nav--left">
+        <img className="nav--icon" src="../../../logo192.png"></img>
+        <h3 className="nav--logotext">ReactFacts</h3>
+      </div>
+      <h4 className="nav--title">React Course - Project 1</h4>
+    </nav>
+  );
+}
